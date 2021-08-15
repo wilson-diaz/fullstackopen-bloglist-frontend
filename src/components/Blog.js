76aefc7b8ba199b5339f53f83f7856931cd39f18
Wilson Diaz <wdfiguereo@gmail.com>
2021-08-14 23:24:15 -0400

    5.7: toggle extra blog post details

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,35 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { visible } from 'ansi-colors'
+import { useState } from 'react'
+
+const Blog = ({blog}) => {
+  const [visible, setVisible] = useState(false)
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author} <button onClick={toggleVisibility}>{visible ? 'hide' : 'view'}</button>
+      </div>
+      <div style={showWhenVisible}>
+        <p>{blog.url}</p>
+        <p>likes {blog.likes} <button>like</button></p>
+        <p>{blog.user.username}</p>
+      </div>
+    </div>  
+  )
+}
 
 export default Blog
